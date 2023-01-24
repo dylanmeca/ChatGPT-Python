fc5d93f13e200455a549e5bc8dc6f6f23f701fcc
Dylan Meca <55631040+dylanmeca@users.noreply.github.com>
2023-01-23 23:38:05 -0500

    Add files via upload

---
+++ ChatGPT-Termux.py
@@ -0,0 +1,37 @@
+# ChatGPT-Python para Termux
+import openai
+from gtts import gTTS
+import os
+
+# OPENAI API KEY
+openai.api_key = ""
+# Lista con despedidas comunes para finalizar el ciclo while
+despedida = ["Adios", "adios", "bye", "Bye", "Hasta luego", "hasta luego"]
+
+while True:
+    # Entrada del usuario y verificacion si la entrada coincide con la lista de despedidas
+    usuario = str(input("tu: "))
+    if usuario in despedida:
+        break
+    else:
+        prompt = (f"{usuario}")
+
+    # Obteniendo respuestas usando la API de OpenAI
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024
+    )
+
+    respuesta = response["choices"][0]["text"]
+    #print("ChatGPT:", response["choices"][0]["text"])
+    # Convirtiendo texto a audio
+    texto = str(respuesta)
+    tts = gTTS(texto, lang="es")
+    tts.save("audio.mp3")
+    # Mostrando la respuesta en pantalla
+    print("ChatGPT: ", respuesta)
+    # Reproduciendo el audio
+    os.system("pulseaudio -D")
+    os.system("play audio.mp3")
+    os.system("pulseaudio -k")
