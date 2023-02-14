5c1463fe6645a989d125dc1452418e472ced3fe8
Dylan Meca <55631040+dylanmeca@users.noreply.github.com>
2023-02-13 20:31:10 -0500

    Update ChatGPT-Python.py

--- ChatGPT-Python.py
+++ ChatGPT-Python.py
@@ -1,21 +1,26 @@
 import openai
 import gradio as gr
 
-# OPENAI API KEY
-openai.api_key = ""
+openai.api_key = None
 
 # Obteniendo respuestas usando la API de OpenAI
-def respuesta_chatgpt(mensaje):
-    prompt = (f"{mensaje}")
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=1024
-    )
-    # Mostrando la respuesta en pantalla
-    respuesta = response["choices"][0]["text"]
-    return respuesta
-
+def respuesta_chatgpt(api_key, mensaje):
+  # OPENAI API KEY
+  openai.api_key = api_key
+  prompt = (f"{mensaje}")
+  response = openai.Completion.create(
+      engine="text-davinci-003",
+      prompt=prompt,
+      max_tokens=1024
+  )
+  # Mostrando la respuesta en pantalla
+  respuesta = response["choices"][0]["text"]
+  return respuesta
+      
 # Entrada del usuario
-chatbot = gr.Interface(fn=respuesta_chatgpt, inputs="text", outputs="text")
+chatbot = gr.Interface(
+    fn=respuesta_chatgpt, 
+    inputs=["text", "text"],
+    outputs="text",
+)
 chatbot.launch()   
